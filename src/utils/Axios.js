7cc6e15c313e8f75017f6fd957242a91ec11ed2e
dubenikhil461 <nikhildubey461@gmail.com>
2025-09-22 00:56:16 +0530

    changes

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const Ax = axios.create({
-    baseURL : "http://127.0.0.1:8000/api",
+    baseURL : "https://agriaibackend1.onrender.com/api",
     withCredentials :true
 })
 
